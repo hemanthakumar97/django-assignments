3aedc4df50e63653753710194fabd8f2d38435c1
Hemanth <hemanth.hku@gmail.com>
2020-08-11 21:00:12 +0530

    p12

--- new/P7/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from myapp import views
-app_name="myapp"
-
-urlpatterns = [
-    path('trail/',views.trial,name="Trial"),
-    path('profile/',views.profile,name="profile"),
-]
