ac3638893d1e8f8ab2107355656f08f6a3b52ad9
Hemanth <hemanth.hku@gmail.com>
2020-08-18 10:55:54 +0530

    settings

--- p12/p12/settings.py
+++ p12/p12/settings.py
@@ -130,4 +130,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hktechlabs@gmail.com'
-EMAIL_HOST_PASSWORD = "hkzpvlmxyptmuyltsgtech"
+EMAIL_HOST_PASSWORD = "hkzpvlmxyptmuyltsgtechk"
