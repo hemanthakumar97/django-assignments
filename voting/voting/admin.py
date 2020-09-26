e542576d2c3f8ff31875ec34d8a4803641d422aa
Hemanth <hemanth.hku@gmail.com>
2020-09-26 11:18:13 +0530

    voting

--- voting/voting/admin.py
+++ voting/voting/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from voting.models import *
-# Register your models here.
+
+
+class CandidateAdminView(admin.ModelAdmin):
+    list_display = ["name", "constituency", "no_votes"]
+
+class VotersAdminView(admin.ModelAdmin):
+    list_display = ["user", "constituency", "is_voted"]
+
 admin.site.register(Constituency)
-admin.site.register(Candidate)
-admin.site.register(Voter)
+admin.site.register(Candidate, CandidateAdminView)
+admin.site.register(Voter, VotersAdminView)
